1ed7bd239f84fe6ed19a2fdfb64681de3fbb5a1b
Raseena-Najeeb <75878014+Raseena-Najeeb@users.noreply.github.com>
2021-08-07 20:06:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,23 +4,23 @@ var i;
 
 function preload(){
   pathImg = loadImage("path.png");
-  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  boyanm = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
   
   createCanvas(400,400);
   
-// Moving background
+
 path=createSprite(200,200);
 path.addImage(pathImg);
-path.velocityY = 4;
+path.velocityY = 3;
 path.scale=1.2;
 
-//creating boy running
+
 boy = createSprite(180,340,30,30);
 boy.scale=0.08;
-boy.addAnimation("JakeRunning",boyImg);
+boy.addAnimation("Running",boyanm);
   
 // create left Boundary
 leftBoundary=createSprite(0,0,100,800);
